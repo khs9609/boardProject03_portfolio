29fa3ac5a6cc18b89d03ec4af0eccf686dc363da
hyo-seop <rlagytjq321@gmail.com>
2021-11-11 05:13:28 +0900

    회원가입 서비스 구축

--- src/main/java/board/web/MemberController.java
+++ src/main/java/board/web/MemberController.java
@@ -5,9 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MemberController {
-
+	
+	/* 로그인 페이지*/
 	@RequestMapping("memberLogin.do")
 	public String loginWrite() {
+		
 		return "member/memberLogin";
+	}
+	
+	/* 회원가입 페이지 */
+	@RequestMapping("memberWrite.do")
+	public String memberWrite() {
+		
+		return "member/memberWrite";
 	}
 }
